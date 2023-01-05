9ffc2fe0079a358a1d42a32e8796ef3c98b502e9
Hassan Nawaz <120159774+nawaz677@users.noreply.github.com>
2023-01-05 19:28:17 +0500

    Add files via upload

---
+++ task01.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+char ch;
+cout<<"Enter any character: ";
+cin>>ch;
+cout<<ch              	 <<endl;
+cout<<ch<<ch          	 <<endl;
+cout<<ch<<ch<<ch      	 <<endl;
+cout<<ch<<ch<<ch<<ch     <<endl;
+cout<<ch<<ch<<ch<<ch<<ch <<endl;
+}
